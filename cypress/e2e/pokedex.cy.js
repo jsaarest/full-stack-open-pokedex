cff1daeebe67a9a00330d73b7827da8b268d930d
jsaarest <39669220+jsaarest@users.noreply.github.com>
2023-05-08 13:16:48 +0300

    change base path

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,14 +1,12 @@
-const API_BASE_PATH = 'http://localhost:5000'
-
 describe('template spec', () => {
   it('front page can be opened', function() {
-    cy.visit(API_BASE_PATH)
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   }),
   it('a specific pokemon page can be visited', function() {
     const pokemon = 'ivysaur'
-    cy.visit(API_BASE_PATH)
+    cy.visit('http://localhost:5000')
     cy.contains(pokemon)
     cy.contains(pokemon).click()
     cy.contains('chlorophyll')
