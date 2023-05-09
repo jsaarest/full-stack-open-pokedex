c14647d1436afeca974a5433eea8740fec1a5de3
jsaarest <39669220+jsaarest@users.noreply.github.com>
2023-05-09 12:12:23 +0300

    Update pokedex.cy.js

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,12 +1,12 @@
 describe('template spec', () => {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5000/')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   }),
   it('a specific pokemon page can be visited', function() {
     const pokemon = 'ivysaur'
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5000/')
     cy.contains(pokemon)
     cy.contains(pokemon).click()
     cy.contains('chlorophyll')
